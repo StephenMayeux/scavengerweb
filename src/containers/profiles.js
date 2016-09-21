96a4740494c94a4597a85d1050570b3a2bc3aee2
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-21 00:03:02 -0400

    style profiles list

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
@@ -11,9 +12,13 @@ class Profiles extends Component {
     if (this.props.profiles.data) {
       return this.props.profiles.data.map(profile => {
         return (
-          <li key={profile._id}>
-            {profile.name}
-          </li>
+          <div className="col-sm-4" key={profile._id}>
+            <div className="panel">
+              <div className="panel-body">
+                <h3><Link to={`/profiles/${profile._id}`} >{profile.name}</Link></h3>
+              </div>
+            </div>
+          </div>
         );
       });
     }
@@ -21,9 +26,11 @@ class Profiles extends Component {
 
   render() {
     return (
-      <ul>
-        {this.renderList()}
-      </ul>
+      <div className="container-fluid">
+        <div className="row">
+          {this.renderList()}
+        </div>
+      </div>
     );
   }
 }
