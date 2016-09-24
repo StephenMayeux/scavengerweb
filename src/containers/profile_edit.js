c052661af1aa96364b80f66b838c5e90ab961cc9
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-24 16:56:55 -0400

    remove redux form

--- src/containers/profile_edit.js
+++ src/containers/profile_edit.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class ProfileEdit extends Component {
@@ -20,41 +21,40 @@ class ProfileEdit extends Component {
     this.setState({ [event.target.name]: event.target.value });
   }
 
-  handleFormSubmit(formProps) {
-    console.log('these are form props', formProps);
-    this.props.editUser(this.props.user.user._id, formProps);
+  handleFormSubmit(event) {
+    event.preventDefault();
+    this.props.editUser(this.props.user.user._id, this.state);
   }
 
   render() {
-    const { handleSubmit, initialValue, fields: { name, email, city, homepage, avatar }} = this.props
     return (
       <div className="container">
         <div className="panel">
           <div className="panel-body">
-            <form className="form-horizontal" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <form className="form-horizontal" onSubmit={this.handleFormSubmit.bind(this)}>
               <legend>Profile Information</legend>
               <div className="form-group">
                 <label className="col-sm-3">Name</label>
                 <div className="col-sm-7">
-                  <input {...name} name="name" className="form-control" autoFocus value={this.state.name} onChange={this.handleChange.bind(this)}/>
+                  <input name="name" className="form-control" value={this.state.name} onChange={this.handleChange.bind(this)}/>
                 </div>
               </div>
               <div className="form-group">
                 <label className="col-sm-3">Email</label>
                 <div className="col-sm-7">
-                  <input {...email} name="email" type="email" className="form-control" value={this.state.email} onChange={this.handleChange.bind(this)}/>
+                  <input name="email" type="email" className="form-control" value={this.state.email} onChange={this.handleChange.bind(this)}/>
                 </div>
               </div>
               <div className="form-group">
                 <label className="col-sm-3">City</label>
                 <div className="col-sm-7">
-                  <input {...city} name="city" className="form-control" value={this.state.city} onChange={this.handleChange.bind(this)}/>
+                  <input name="city" className="form-control" value={this.state.city} onChange={this.handleChange.bind(this)}/>
                 </div>
               </div>
               <div className="form-group">
                 <label className="col-sm-3">Website</label>
                 <div className="col-sm-7">
-                  <input {...homepage} name="homepage" className="form-control" value={this.state.homepage} onChange={this.handleChange.bind(this)}/>
+                  <input name="homepage" className="form-control" value={this.state.homepage} onChange={this.handleChange.bind(this)}/>
                 </div>
               </div>
               <div className="form-group">
@@ -80,7 +80,4 @@ function mapStateToProps(state) {
   return { user: state.profiles }
 }
 
-export default reduxForm({
-  form: 'edit',
-  fields : ['name', 'email', 'city', 'homepage', 'avatar']
-}, mapStateToProps, actions)(ProfileEdit);
+export default connect(mapStateToProps, actions)(ProfileEdit);
